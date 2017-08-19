6fa3e5328321a2a5eb1d4710ed5e7610a544bf86
rachana <sairachana.siddam@gmail.com>
2017-08-19 01:26:18 -0400

    Add comments

--- Flights_files/utag.522.js
+++
@@ -1,5 +0,0 @@
-//tealium universal tag - utag.522 ut4.0.201602042317, Copyright 2016 Tealium.com Inc. All Rights Reserved.
-try{(function(id,loader,u){u=utag.o[loader].sender[id]={};u.ev={'view':1};u.qsp_delim="&";u.kvp_delim="=";u.data={};u.data.pid="1449";u.map={"adara_pg":"pg","mhe":"u"};u.extend=[function(a,b){try{if(b['pageType']=='hp'){b['adara_pg']='hm'}}catch(e){utag.DB(e)}}];u.send=function(a,b){if(u.ev[a]||typeof u.ev.all!="undefined"){for(c=0;c<u.extend.length;c++){try{d=u.extend[c](a,b);if(d==false)return}catch(e){}};var c,d,e,f;c=[];for(d in utag.loader.GV(u.map)){if(typeof b[d]!="undefined"&&b[d]!=""){e=u.map[d].split(",");for(f=0;f<e.length;f++){c.push(e[f]+u.kvp_delim+encodeURIComponent(b[d]))
-u.data[e[f]]=encodeURIComponent(b[d]);}}}
-u.base_url="//tag.yieldoptimizer.com/ps/ps?t=s&p="+u.data.pid+"&";u.img=new Image();u.img.src=u.base_url+c.join(u.qsp_delim);}}
-utag.o[loader].loader.LOAD(id);})('522','pcln.main');}catch(e){}
--- spaceReduction.js
+++ spaceReduction.js
@@ -1,3 +1,4 @@
+// Sort list of lists using the first column
 function compareFirstColumn(a, b) {
     if (a[0] === b[0]) {
         return 0;
@@ -7,6 +8,7 @@ function compareFirstColumn(a, b) {
     }
 }
 
+// Sort list of lists using the second column
 function compareSecondColumn(a, b) {
     if (a[1] === b[1]) {
         return 0;
@@ -16,8 +18,10 @@ function compareSecondColumn(a, b) {
     }
 }
 
+// Get vertical lines that divide the visual blocks
 function verticalSegmentation(visual_blocks){
     visual_blocks.sort(compareSecondColumn);
+    // vertical_lines 2d array - [[start1, end1], [start2, end2]]
     var n_visual_blocks = visual_blocks.length, vertical_lines = [], i, size;
     for(i=0; i<n_visual_blocks; i++){
         size = vertical_lines.length;
@@ -28,15 +32,17 @@ function verticalSegmentation(visual_blocks){
             vertical_lines.push([visual_blocks[i][1],visual_blocks[i][3]]);
         }
     }
+    // convert vertical_lines 2d array to 1d array
     vertical_lines = [].concat.apply([], vertical_lines);
     vertical_lines = vertical_lines.filter(function(item, pos) { return vertical_lines.indexOf(item) == pos;});
     return vertical_lines;
 }
 
-
+// Get horizontal lines that divide the visual blocks
 function horizontalSegmentation(visual_blocks){
     visual_blocks.sort(compareFirstColumn);
     var n_visual_blocks = visual_blocks.length, horizontal_lines = [], i, size;
+     // horizontal_lines 2d array - [[start1, end1], [start2, end2]]
     for(i=0; i<n_visual_blocks; i++){
         size = horizontal_lines.length;
         if(size != 0 && horizontal_lines[size-1][0] <= visual_blocks[i][0] && visual_blocks[i][0] < horizontal_lines[size-1][1]){
@@ -46,11 +52,13 @@ function horizontalSegmentation(visual_blocks){
             horizontal_lines.push([visual_blocks[i][0],visual_blocks[i][2]]);
         }
     }
+    // convert horizontal_lines 2d array to 1d array
     horizontal_lines = [].concat.apply([], horizontal_lines);
     horizontal_lines = horizontal_lines.filter(function(item, pos) { return horizontal_lines.indexOf(item) == pos;});
     return horizontal_lines;
 }
 
+// Run Segmentation algorithm, get block - (top, left, right, bottom) and div_block - (divs grouped together based on blocks)
 function runSegmentation(visual_blocks){
     var blocks, div_blocks,
         vertical_lines = verticalSegmentation(visual_blocks),
@@ -61,6 +69,7 @@ function runSegmentation(visual_blocks){
     return [blocks, div_blocks];
 }
 
+// Segment the element and get minimum width and height of the element
 function getMinimumWidthHeight(container, elmWidths, elmHeights){
 //    visual_blocks = [[2, 3, 4, 5], [6, 9, 9, 11], [3, 7, 5, 9], [6, 4, 8, 6], [3, 3, 5, 5]];
     var blocks, div_blocks, alignment, mWidth, mHeight,
@@ -75,6 +84,7 @@ function getMinimumWidthHeight(container, elmWidths, elmHeights){
     return [mWidth, mHeight];
 }
 
+// Get index of block in blocks (list of lists)
 function getBlockIndex(blocks, v1, h1, v2, h2){
     var block;
     for(var i=0; i<blocks.length; i++){
@@ -86,6 +96,7 @@ function getBlockIndex(blocks, v1, h1, v2, h2){
     return -1;
 }
 
+// Get the grids - (contains atleast one html element)
 function getBlocks(visual_blocks, vertical, horizontal){
     var blocks = [], div_blocks = {}, h1, h2, v1, v2, index;
     for(var i = 0; i < visual_blocks.length; i++) {
@@ -104,6 +115,7 @@ function getBlocks(visual_blocks, vertical, horizontal){
     return [blocks, div_blocks];
 }
 
+// Get the smallest number closer to num from arr
 function closestFloorNum(num, arr){
     var len = arr.length;
     for(var i = 1; i < len; i++) {
@@ -114,6 +126,7 @@ function closestFloorNum(num, arr){
     return arr[len-1];
 }
 
+// Get the largest number closer to num from arr
 function closestCeilNum(num, arr){
     var len = arr.length;
     for(var i = 0; i < len-1; i++) {
@@ -124,6 +137,7 @@ function closestCeilNum(num, arr){
     return arr[len-1];
 }
 
+// Get dimensions of the immediate children
 function getVisualBlocks(container){
     var elm, position, visual_block, visual_blocks = [];
     for (var i = 0; i < container.childElementCount; i++) {
@@ -143,8 +157,11 @@ function getVisualBlocks(container){
     return visual_blocks;
 }
 
+// Get alignment of immediate children, [1,2] represents 1, 2 are horizontal to each other
+// [[1],[2]] represents 1,2 are vertical to each other
 function getAlignment(blocks, div_blocks){
     var alignment = [[0]], i, j, k, db, b;
+    // alignment - contains index of children grouped together
     if(blocks.length == 1){
         for(i=1; i<div_blocks[0].length; i++){
             if(div_blocks[0][i-1][1] == div_blocks[0][i][1] && div_blocks[0][i-1][3] == div_blocks[0][i][3]){
@@ -163,10 +180,12 @@ function getAlignment(blocks, div_blocks){
         }
     }
 
+    // Change index of children to children element
     for(i=0; i<alignment.length; i++){
         for(j=0; j<alignment[i].length; j++){
             k = alignment[i][j];
             if(blocks.length > 1 && div_blocks[k].length > 1){
+                // If blocks has more that one html element, segment the block and get alignment
                 var segmentedBlocks = runSegmentation(div_blocks[k]);
                 b = segmentedBlocks[0];
                 db = segmentedBlocks[1];
@@ -182,6 +201,7 @@ function getAlignment(blocks, div_blocks){
     return alignment;
 }
 
+// Get minimum height and width of parent, given alignment of children
 function getElementWidthHeight(alignment, elmWidths, elmHeights){
     var pw, ph, pWidth = 0, pHeight = 0;
     for(var i=0; i<alignment.length; i++){
@@ -189,11 +209,13 @@ function getElementWidthHeight(alignment, elmWidths, elmHeights){
             var pWH = getElementWidthHeight(alignment[i][0], elmWidths, elmHeights);
             pw = pWH[0];
             ph = pWH[1];
+            // vertical elements
             pWidth = Math.max(pw, pWidth);
             pHeight += ph;
         }else if(alignment[i] && alignment[i] instanceof Array){
             var w = 0, h = 0;
             for(var j=0; j<alignment[i].length; j++){
+                // horizontal elements
                 w += elmWidths[alignment[i][j]];
                 h = Math.max(elmHeights[alignment[i][j]], h);
 
@@ -208,24 +230,29 @@ function getElementWidthHeight(alignment, elmWidths, elmHeights){
     return [pWidth, pHeight];
 }
 
+// Revert back the changes made to CSS
 function revertBack(element){
     element.style.cssText = element.previousCSS;
 }
 
-//variable reductionFactor lies between 0 and 1
+// Reduce Space of an element based on minimum height, width.
+// Update padding, margin, position, border of element
+// Update font size
+// Variable reductionFactor lies between 0 and 1
 function reduceSpace(element, mWidth, mHeight, reductionFactor){
 //    var previousCSS = '';
 //    for(var i = 0; i < css.length; i++){
 //        previousCSS += css[i] + ':' + css.getPropertyValue(css[i]) + ';';
 //    }
 //    If below declaration of previousCSS fails, go for the above
-//    if(!$(element).is(':visible') || $(element).css('display') == 'none' || $(element).css('visibility') == 'hidden'){
+//    if(!$(element).is(':visible') || $(element).css('display') == 'none' || $(element).css('visibility') == 'hidden')
     console.log(element);
     var css = getComputedStyle(element),
-        width = parseInt(css.getPropertyValue('width'))+1,
+        width = parseInt(css.getPropertyValue('width'))+1, // Handle non int values by adding 1
         height = parseInt(css.getPropertyValue('height'))+1;
 
     element.previousCSS = css.cssText;
+    // Handle auto
     if(css.getPropertyValue('width') == 'auto'){
         width = element.getBoundingClientRect().width;
     }
@@ -235,6 +262,7 @@ function reduceSpace(element, mWidth, mHeight, reductionFactor){
     }
 
     if(element.tagName == 'INPUT' || element.tagName == 'SELECT'){
+        // Reduce input and select elements by atleast 0.1
         width *= 0.9;
         height *= 0.9;
     }
@@ -262,34 +290,37 @@ function reduceSpace(element, mWidth, mHeight, reductionFactor){
     var border = updateBorder(element, css, reductionFactor);
     borderVertical = border[0];
     borderHorizontal = border[1];
+
+    // Set font size and width of text
     if(element.innerText && element.innerText.replace(/\s/g,'') != '' && css.getPropertyValue('font-size')){
         if(element.firstChild && element.firstChild.nodeValue && element.firstChild.nodeValue.replace(/\s/g,'') != '' ){
             element.style.fontSize = getFontSize(parseInt(css.getPropertyValue('font-size'))) + 'px';
             css = getComputedStyle(element);
-//            TODO: If get text width function is reliable(which is not reliable now), the space reduction algorithm will be more effective
-//            var wh = getTextWidth(element.firstChild.nodeValue, css.getPropertyValue('font'));
-//            width = wh[0];
-//            height = wh[1];
+            // TODO: If get text width function is reliable(which is not reliable now), the space reduction algorithm will be more effective
+            // var wh = getTextWidth(element.firstChild.nodeValue, css.getPropertyValue('font'));
+            // width = wh[0];
+            // height = wh[1];
             var wh = measureText(element.firstChild.nodeValue, css.getPropertyValue('font-size'), element.style);
             width = wh.width;
-//            Hardcoded: Adding 40px
+            // Hardcoded: Adding 40px
             element.style.width = width + 40 + 'px';
-//            element.style.height = height + 'px';
+            // element.style.height = height + 'px';
             css = getComputedStyle(element);
             width = parseInt(css.getPropertyValue('width'))+1;
-//            height = parseInt(css.getPropertyValue('height'))+1;
+            // height = parseInt(css.getPropertyValue('height'))+1;
         }
     }
 
     return [paddingHorizontal+marginHorizontal+width, paddingVertical+marginVertical+height];
 }
 
+// Start of algorithm (Begin here)
 function spaceReduction(query){
     var container = document.querySelector(query);
 //    var container = document.getElementById(query);
-    if(container)
+    if(container){
         if(container.id && container.id == 'navbar'){
-            //    Hardcoded: Condition id nav bar
+            // Hardcoded: Condition id nav bar for amazon
             return 'Space reduction algorithm run successful';
         }
         getWidthHeightContainer(container);
@@ -298,6 +329,7 @@ function spaceReduction(query){
     return 'Space reduction algorithm run failed';
 }
 
+// Get width height of the updated container
 function getWidthHeightContainer(container) {
     var elmWidths = {}, elmHeights = {},
         w, h, mWidth, mHeight, elm, css, count = 0;
@@ -316,29 +348,34 @@ function getWidthHeightContainer(container) {
     }
 
     if(!isVisible(container) || count == 0 || container.tagName == 'SELECT'){
+        // Set by default
         mWidth = 0;
         mHeight = 0;
     }else if(count == 1){
+        // Don't perform segmentation if there is only one child element
         css = getComputedStyle(container.children[0]);
         mWidth = parseInt(css.getPropertyValue('width'))+1;
         mHeight = parseInt(css.getPropertyValue('height'))+1;
     }else{
+        // Start segmentation algorithm and get minimum height, width
         var widthHeight = getMinimumWidthHeight(container, elmWidths, elmHeights);
         mWidth = widthHeight[0];
         mHeight = widthHeight[1];
     }
     console.log([elmWidths,elmHeights]);
 
-//    Hardcoded: Condition for priceline packages page
     if(container.className == 'VacationPackagesSearchForm__flight-passengers mobile-seti'){
+        // Hardcoded: Condition for priceline packages page
         css = getComputedStyle(container);
         mHeight = css.getPropertyValue('height');
     }
 
+    // Reduce space of current container by using the min height, width obtained
     var widthHeight = reduceSpace(container, mWidth, mHeight, 0.3);
     return [widthHeight[0], widthHeight[1]];
 }
 
+// Get font size based on current font
 function getFontSize(size){
     if(size < 12){
         return size*1.3;
@@ -349,6 +386,7 @@ function getFontSize(size){
     }
 }
 
+// Get size of width/height, irrespective of % or px value
 function getSize(size, units){
     if(units){
         return size + '%';
@@ -357,6 +395,7 @@ function getSize(size, units){
     }
 }
 
+// Set width/height to minSize if the condition is satisfied
 function minimizeWidthHeight(element, minSize, size, quantity){
     if(minSize && minSize > 0 && minSize < size){
         size = minSize;
@@ -365,11 +404,13 @@ function minimizeWidthHeight(element, minSize, size, quantity){
     return size;
 }
 
+// Set min-height, min-width to 0px
 function updateMinimumWidthHeight(element){
     element.style.minHeight = '0px';
     element.style.minWidth = '0px';
 }
 
+// Update padding of an element
 function updatePadding(element, css, reductionFactor){
     var pb = css.getPropertyValue('padding-bottom'),
         pt = css.getPropertyValue('padding-top'),
@@ -387,6 +428,7 @@ function updatePadding(element, css, reductionFactor){
     return [paddingBottom+paddingTop, paddingRight+paddingLeft];
 }
 
+// Update margin of an element
 function updateMargin(element, css, reductionFactor){
     var mb = css.getPropertyValue('margin-bottom'),
         mt = css.getPropertyValue('margin-top'),
@@ -404,7 +446,9 @@ function updateMargin(element, css, reductionFactor){
     return [marginBottom+marginTop, marginRight+marginLeft];
 }
 
+// Update position of an element
 function updatePosition(element, css, reductionFactor){
+    // Hardcoded reduction factor
     reductionFactor = 0.9;
     if(element.className == 'global-header-nav-product-list'){
         reductionFactor = 1.2;
@@ -425,7 +469,9 @@ function updatePosition(element, css, reductionFactor){
     return [bottom+top, right+left];
 }
 
+// Update border of an element
 function updateBorder(element, css, reductionFactor){
+    // Hardcoded reduction factor
     reductionFactor = 0.9;
     var bb = css.getPropertyValue('border-bottom-width'),
         bt = css.getPropertyValue('border-top-width'),
@@ -443,6 +489,7 @@ function updateBorder(element, css, reductionFactor){
     return [borderBottom+borderTop, borderRight+borderLeft];
 }
 
+// Check if a html element is visible or not
 function isVisible(elem) {
     if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
     var style = getComputedStyle(elem);
@@ -471,6 +518,8 @@ function isVisible(elem) {
 }
 
 
+// Get width of text based on text content and font
+// Works in Chrome
 function getTextWidth(text, font) {
     // re-use canvas object for better performance
     var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement('canvas'));
@@ -480,7 +529,7 @@ function getTextWidth(text, font) {
     return [metrics.width, metrics.height];
 }
 
-
+// Works in IE
 function measureText(pText, pFontSize, pStyle) {
     var lDiv = document.createElement('div');
 
